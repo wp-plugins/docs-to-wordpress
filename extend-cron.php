065dc669c52b64881f784805730d7a93a58d4b8a
wpdavis <wpdavis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-24 15:04:14 +0000

    Fixing extend cron
    git-svn-id: https://plugins.svn.wordpress.org/docs-to-wordpress/tags/0.4-beta@1053445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- extend-cron.php
+++ extend-cron.php
@@ -6,26 +6,21 @@ Author URI: http://wpdavis.com/
 Version: 0.4-beta
 */
 
-register_activation_hook( __FILE__, 'dtwp_schedule_event' );
-
 //First, allow the cron to run every minute
-add_filter('cron_schedules', 'dtwp_more_reccurences');
-function dtwp_more_reccurences() {
-        return array( 'min' => array( 'interval' => 60, 'display' => 'Every Minute' ) );
+add_filter( 'cron_schedules', 'dtwp_more_reccurences' );
+function dtwp_more_reccurences( $schedules ) {
+        $schedules[ 'min' ] = array( 'interval' => 60, 'display' => 'Every Minute' );
+        return $schedules;
 }
 
-add_action( 'dtwp_cronjob', 'dtwp_check_gdocs' );
+//On activation, add the recurring cron job
+register_activation_hook( __FILE__, 'dtwp_schedule_event' );
 function dtwp_schedule_event() {
         wp_schedule_event( time(), 'min', 'dtwp_cronjob' );
 }
 
-
-//Deactivation
-register_deactivation_hook(__FILE__, 'dtwp_deactivate_cron');
-function dtwp_deactivate_cron() {
-        wp_clear_scheduled_hook( 'dtwp_cronjob' );
-}
-
+//Run the cron job
+add_action( 'dtwp_cronjob', 'dtwp_check_gdocs' );
 function dtwp_check_gdocs( ) {
 	//Init the Docs to WP
 	$docs_to_wp = new Docs_To_WP();
@@ -36,4 +31,11 @@ function dtwp_check_gdocs( ) {
 	//We're just going to call one function:
 	$docs_to_wp->retrieve_docs_for_web( $gdClient, DOCSTOWP_ORIGIN, DOCSTOWP_DESTINATION );
 
+}
+
+
+//Deactivation
+register_deactivation_hook(__FILE__, 'dtwp_deactivate_cron');
+function dtwp_deactivate_cron() {
+        wp_clear_scheduled_hook( 'dtwp_cronjob' );
 }
